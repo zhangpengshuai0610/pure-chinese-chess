bbb8a3042218c7b79b97d101bb127898f7972d9f
ChessDev <player@chinese-chess.dev>
2026-07-29 17:08:53 +0800

    Rename to pure-chinese-chess

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/chinese-chess/',
+  base: '/pure-chinese-chess/',
   plugins: [react(), tailwindcss()],
 })
